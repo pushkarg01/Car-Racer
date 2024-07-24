14fea7d3c5f20b103ce20dcf0e712558b27e28e1
Pushkar Gupta <pushkargupta2901@gmail.com>
2024-07-24 14:55:05 +0530

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,7 +15,6 @@ public class PlayerController : MonoBehaviour
        
     }
 
-    // Update is called once per frame
     void Update()
     {
             myPosition.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
